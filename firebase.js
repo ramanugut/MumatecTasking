9d46bbb2a3cea86351d1713f953710c2c7ca2716
Thembuluwo Ramanugu <42994454+ramanugut@users.noreply.github.com>
2025-06-06 13:41:51 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,16 +3,15 @@ import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.23.0/firebas
 import { getAuth } from 'https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js';
 import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDT01V2niUl83vaYXUEukc5fGzUppwfQEI",
-  authDomain: "mumatectasking.firebaseapp.com",
-  projectId: "mumatectasking",
-  storageBucket: "mumatectasking.firebasestorage.app",
-  messagingSenderId: "10382608024",
-  appId: "1:10382608024:web:f8a04a0196b97e6b0f32ed",
-  measurementId: "G-09GVTSPW3W"
-
-};
+  const firebaseConfig = {
+    apiKey: "AIzaSyDT01V2niUl83vaYXUEukc5fGzUppwfQEI",
+    authDomain: "mumatectasking.firebaseapp.com",
+    projectId: "mumatectasking",
+    storageBucket: "mumatectasking.firebasestorage.app",
+    messagingSenderId: "10382608024",
+    appId: "1:10382608024:web:f8a04a0196b97e6b0f32ed",
+    measurementId: "G-09GVTSPW3W"
+  };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
