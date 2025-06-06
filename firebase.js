658e7b0f8d4131a1661ea6979cf16c284c8f718a
Thembuluwo Ramanugu <42994454+ramanugut@users.noreply.github.com>
2025-06-06 13:43:51 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,13 +4,13 @@ import { getAuth } from 'https://www.gstatic.com/firebasejs/9.23.0/firebase-auth
 import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js';
 
   const firebaseConfig = {
-    apiKey: "AIzaSyDT01V2niUl83vaYXUEukc5fGzUppwfQEI",
-    authDomain: "mumatectasking.firebaseapp.com",
-    projectId: "mumatectasking",
-    storageBucket: "mumatectasking.firebasestorage.app",
-    messagingSenderId: "10382608024",
-    appId: "1:10382608024:web:f8a04a0196b97e6b0f32ed",
-    measurementId: "G-09GVTSPW3W"
+  apiKey: "AIzaSyDT01V2niUl83vaYXUEukc5fGzUppwfQEI",
+  authDomain: "mumatectasking.firebaseapp.com",
+  projectId: "mumatectasking",
+  storageBucket: "mumatectasking.firebasestorage.app",
+  messagingSenderId: "10382608024",
+  appId: "1:10382608024:web:0d94eae7dcb524b40f32ed",
+  measurementId: "G-57MJ7G5HX2"
   };
 
 const app = initializeApp(firebaseConfig);
